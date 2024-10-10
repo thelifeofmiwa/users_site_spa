ab678abf7f9b97bc93b45a7eb06b2b76d5c5df3e
MishaAlexandrov <mikesperanskii@gmail.com>
2024-10-10 18:54:19 +0300

    final correct

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,60 +1,40 @@
 <template>
+  <div class="w-full max-w-[1920px] flex flex-wrap justify-between mx-auto gap-y-4 mt-3 px-4">
     <div
-        class="w-[1900px] flex flex-wrap justify-between mx-auto gap-x-1 gap-y-4 mt-3"
+      class="w-[49%] h-[360px] border rounded-md flex flex-col justify-between items-center p-4 mb-"
+      v-for="user in users"
+      :key="user.name"
     >
-        <div
-            class="w-[945px] h-[360px] border rounded-md flex flex-col justify-between items-center p-2"
-            v-for="user in users"
-            :key="user.name"
-        >
-            <div class="w-200 h-200 my-5 border rounded-[3px]">
-                <img :src="getGravatarUrl(user.email)" :alt="user.username" />
-            </div>
-            <div>
-                <b>{{ user.name }}</b>
-            </div>
-            <div>{{ user.email }}</div>
-            <button
-                class="bg-[rgba(59,130,246,1)] text-white border border-[rgba(59,130,246,1)] rounded-md px-3 py-1 mb-2 mr-2 self-end"
-                @click="showModal(user)"
-            >
-                Подробнее
-            </button>
-        </div>
-        <!-- <user-item :users="users"></user-item> -->
+      <div class="w-[150px] h-[150px] my-5 border rounded-[3px] overflow-hidden">
+        <img :src="getGravatarUrl(user.email)" :alt="user.username" class="w-full h-full object-cover" />
+      </div>
+      <div class="text-center">
+        <b>{{ user.name }}</b>
+        <div>{{ user.email }}</div>
+      </div>
+      <button
+        class="bg-[rgba(59,130,246,1)] text-white border border-[rgba(59,130,246,1)] rounded-md px-3 py-1 mb-2 self-end"
+        @click="showModal(user)"
+      >
+        Подробнее
+      </button>
     </div>
+  </div>
+  <div
+    class="modal fixed inset-0 flex items-center justify-center bg-black bg-opacity-10"
+    v-if="modalVisible"
+    @click="hideModal"
+  >
     <div
-        class="modal fixed inset-0 flex items-center justify-center bg-black bg-opacity-10"
-        v-if="modalVisible"
-        @click="hideModal"
+      class="bg-white w-[800px] max-h-[800px] overflow-auto p-5 rounded-lg shadow-lg"
+      @click.stop
     >
-        <div
-            class="bg-white w-[800px] max-h-[800px] overflow-auto p-5 rounded-lg shadow-lg"
-            @click.stop
-        >
-            <h3 class="text-lg font-bold mb-4">Просмотр</h3>
-            {
-            <div
-                class="space-y-2"
-                v-for="(value1, key1) in selectedUser"
-                :key="key1"
-            >
-                {{ key1 }}: {{ value1 }}
-                <!-- <div v-if="isObject(value1)">
-                        "{{ key1 }}":
-                        <div v-for="(key2, value2) in value1">
-                            
-                            "{{ key2 }}": {{ value2 }}
-                        </div>
-                    </div>
-                    <div v-if="!isObject(value1)">
-                        "{{ key1 }}": {{ value1 }}
-                    </div> -->
-            </div>
-            }
-        </div>
-        <!-- <project-dialog v-model="modalVisible">{{ selectedUser }}</project-dialog> -->
+      <h3 class="text-lg font-bold mb-4">Просмотр</h3>
+      <div class="space-y-2" v-for="(value1, key1) in selectedUser" :key="key1">
+        {{ key1 }}: {{ value1 }}
+      </div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
--- src/components/UserPageComposition.vue
+++ src/components/UserPageComposition.vue
@@ -1,73 +1,75 @@
 <template>
     <div class="min-h-screen flex flex-col">
-        <div class="w-max relative">
-            <h1 class="text-3xl font-sans font-bold pl-4 py-6 text-gray-600">
-                Пользователи
-            </h1>
-        </div>
+      <div class="w-full relative">
+        <h1 class="text-3xl font-sans font-bold pl-4 py-6 text-gray-600">
+          Пользователи
+        </h1>
+      </div>
+      <div class="w-full max-w-[1920px] min-h-6 p-2 flex justify-between items-center bg-gray-200 px-6">
+        <project-input
+          v-model="searchQuery"
+          placeholder="Search by Name"
+          class="w-[300px] placeholder-gray-500"
+        />
+        <project-select
+          v-model="selectedSort"
+          :options="sortOptions"
+          class="w-full max-w-xs ml-4"
+        />
+      </div>
+      <div class="flex-grow">
+        <user-list :users="sortedAndSearchedUsers" />
+      </div>
+      <div class="flex justify-center p-2 mt-auto mb-4 space-x-1">
         <div
-            class="w-full max-w-[1920px] min-h-6 p-2 flex justify-between bg-gray-200"
+          class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200 bg-white text-gray-500 hover:bg-gray-200"
+          @click="changePage(1)"
+          :disabled="page === 1"
         >
-            <project-input
-                v-model="searchQuery"
-                placeholder="Search by Name"
-                class="placeholder-gray-500"
-            />
-            <project-select v-model="selectedSort" :options="sortOptions" />
+          <span class="text-lg">&#171;</span>
         </div>
-        <div>
-            <user-list :users="sortedAndSearchedUsers" />
+  
+        <div
+          class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200 bg-white text-gray-500 hover:bg-gray-200"
+          @click="changePage(page - 1)"
+          :disabled="page === 1"
+        >
+          <span class="text-lg">&#8249;</span>
         </div>
-        <div class="flex justify-center p-2 mt-auto mb-4 space-x-1">
-            <div
-                class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200 bg-white text-gray-500 hover:bg-gray-200"
-                @click="changePage(1)"
-                :disabled="page === 1"
-            >
-                <span class="text-lg">&#171;</span>
-            </div>
-
-            <div
-                class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200 bg-white text-gray-500 hover:bg-gray-200"
-                @click="changePage(page - 1)"
-                :disabled="page === 1"
-            >
-                <span class="text-lg">&#8249;</span>
-            </div>
-
-            <div
-                v-for="pageNumber in totalPages"
-                :key="pageNumber"
-                class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200"
-                :class="{
-                    'bg-black text-white': page === pageNumber,
-                    'bg-white text-gray-500': page !== pageNumber,
-                    'hover:bg-gray-200 hover:text-black': page !== pageNumber,
-                }"
-                @click="changePage(pageNumber)"
-            >
-                {{ pageNumber }}
-            </div>
-
-            <div
-                class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200 bg-white text-gray-500 hover:bg-gray-200"
-                @click="changePage(page + 1)"
-                :disabled="page === totalPages"
-            >
-                <span class="text-lg">&#8250;</span>
-            </div>
-
-            <div
-                class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200 bg-white text-gray-500 hover:bg-gray-200"
-                @click="changePage(totalPages)"
-                :disabled="page === totalPages"
-            >
-                <span class="text-lg">&#187;</span>
-            </div>
+  
+        <div
+          v-for="pageNumber in totalPages"
+          :key="pageNumber"
+          class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200"
+          :class="{
+            'bg-black text-white': page === pageNumber,
+            'bg-white text-gray-500': page !== pageNumber,
+            'hover:bg-gray-200 hover:text-black': page !== pageNumber,
+          }"
+          @click="changePage(pageNumber)"
+        >
+          {{ pageNumber }}
         </div>
+  
+        <div
+          class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200 bg-white text-gray-500 hover:bg-gray-200"
+          @click="changePage(page + 1)"
+          :disabled="page === totalPages"
+        >
+          <span class="text-lg">&#8250;</span>
+        </div>
+  
+        <div
+          class="cursor-pointer w-10 h-10 flex items-center justify-center rounded-full border-none transition duration-200 bg-white text-gray-500 hover:bg-gray-200"
+          @click="changePage(totalPages)"
+          :disabled="page === totalPages"
+        >
+          <span class="text-lg">&#187;</span>
+        </div>
+      </div>
     </div>
-</template>
-
+  </template>
+  
 <script lang="ts">
 import { defineComponent } from "vue";
 import UserList from "./UserList.vue";
